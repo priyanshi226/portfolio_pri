f4f9742009456a28e6cbc2669755b05eb0a9fec0
Priyanshi Gupta <priyanshi@pratinidhi.ai>
2026-04-14 23:02:59 +0530

    added base path fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio_pri/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
